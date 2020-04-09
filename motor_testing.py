ed5cf144c198826196045857822701852e5ef5b6
Benson <bensonja@ad.msoe.edu>
2020-04-09 17:40:49 -0500

    Reverted changes to motor_testing.py

--- motor_testing.py
+++ motor_testing.py
@@ -19,7 +19,7 @@ counter = 0
 # assuming increments for one rotation is 4000
 # full_rotation = 107405
 
-delay = .1
+delay = .001
 
 fsm = state_machine.Motor_State_Machine()
 fsm.update(state_machine.Motor_State.s0, a, b, z, start=True, stop=False, pause=False)
@@ -38,13 +38,13 @@ while(revolutions > 0):
     up_down, count_enable, no_error, at_zero = fsm.update(fsm.future_state, pin_a.is_pressed, pin_b.is_pressed, pin_z.is_pressed, start=True, stop=False, pause=False)
     if at_zero and prev_at_zero:
         revolutions -= 1
-    elif not count_enable:
+    if not no_error:
+        errors += 1
+    if not count_enable:
         if up_down:
             counter -= 1
         else:
             counter += 1
-    if not no_error:
-        errors += 1
     prev_at_zero = at_zero
     print(str(fsm.future_state)+ " " + str(int(pin_a.is_pressed)) + " " + str(int(pin_b.is_pressed)) + " " + str(int(pin_z.is_pressed)) + " " + str(counter))
 print("Errors: " + str(errors))
